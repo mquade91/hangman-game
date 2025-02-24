b4647f59b2bd5ba35b7ec1bb6ab8384bdf29b85d
Matt Quade <quadem09@gmail.com>
2025-02-24 18:56:59 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Hangman from './Hangman';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hangman
+      </header>
+       <Hangman />      
+    </div>
+  );
+}
+
+export default App;
