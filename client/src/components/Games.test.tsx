2dbae8e82ca981e09a2876356a189319958f329d
Matt Quade <quadem09@gmail.com>
2025-06-16 13:14:00 -0400

    add tests

--- client/src/components/Games.test.tsx
+++ client/src/components/Games.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import Games from './Games';
 
@@ -7,4 +8,17 @@ test('Games component loads h1 header', () => {
   render(<Games games={[]} />);
   const linkElement = screen.getByText(/Games/i);
   expect(linkElement).toBeInTheDocument();
+});
+test('renders Hangman when Hangman button is clicked', async () => {
+  render(<Games games={[]} />);
+  const hangmanButton = screen.getByText(/Hangman/i);
+  await userEvent.click(hangmanButton);
+  expect(screen.getByText(/Hangman/i)).toBeInTheDocument();
+});
+
+test('renders Tic Tac Toe when Tic Tac Toe button is clicked', async () => {
+  render(<Games games={[]} />);
+  const ticTacToeButton = screen.getByText(/Tic Tac Toe/i);
+  await userEvent.click(ticTacToeButton);
+  expect(screen.getByText(/Tic Tac Toe/i)).toBeInTheDocument();
 });
